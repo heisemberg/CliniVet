19d0448809e85f58994c4e2f65ea9bb3b2824af3
heisemberg <heissen1122@gmail.com>
2024-12-18 22:33:39 -0500

    Modificacion vista pet consulta admin

--- appClinivet/views/pet_view.py
+++ appClinivet/views/pet_view.py
@@ -13,21 +13,18 @@ class PetViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
-            return Pet.objects.all()
+            # Filtrar por el negocio del usuario autenticado
+            return Pet.objects.filter(owner__business=user.business)
         return Pet.objects.filter(owner=user.client)
 
     def perform_create(self, serializer):
         if not self.request.user.is_staff:
             serializer.save(owner=self.request.user.client)
         else:
-            serializer.save()
+            # Asignar el negocio del usuario autenticado al crear una mascota
+            serializer.save(owner=self.request.user.client, business=self.request.user.business)
 
     def perform_update(self, serializer):
         if not self.request.user.is_staff and serializer.instance.owner != self.request.user.client:
             raise PermissionDenied("You do not have permission to edit this pet.")
         serializer.save()
-
-    def perform_destroy(self, instance):
-        if not self.request.user.is_staff:
-            raise PermissionDenied("You do not have permission to delete this pet.")
-        instance.delete()
